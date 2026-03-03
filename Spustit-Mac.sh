46d887148020e8f0ce842a241e5dd2b9b0775e22
VFosenbauer <caelebs87@gmail.com>
2026-03-03 22:22:42 +0100

    Delete Spustit-Mac.sh

--- Spustit-Mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$DIR"
-echo "Spoustim IFC Materiály Extractor na http://localhost:8765 ..."
-(sleep 1 && open "http://localhost:8765") &
-python3 -m http.server 8765
